6f7e5c9bc4474a9e95ad6b51fd4b5671d5fbf522
Anthony Finix <anthonyfinix@gmail.com>
2021-09-04 19:07:51 +0530

    add register in root to auth service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,26 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthHttpService } from './auth/http.service';
+import { createUserResponseDto } from './user/dto/createResponse.dto';
+import { CreateUserDto } from './user/dto/createUser.dto';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly authHttpService: AuthHttpService,
+  ) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+
+  @Post('/register')
+  async register(@Body() body: CreateUserDto): Promise<createUserResponseDto> {
+    let response = await this.authHttpService.axios.post('/user/register', body);
+    return response.data;
   }
 
 }
